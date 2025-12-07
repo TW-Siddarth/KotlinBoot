005d3470fdcfef03464aeaeaf23db7460d9f7d57
Siddarth Iyer <siddarth.iyer@thoughtworks.com>
2025-12-08 04:01:18 +0530

    Inline value class

---
+++ src/main/kotlin/special_classes/InlineValueClasses.kt
@@ -0,0 +1,30 @@
+package special_classes
+
+@JvmInline
+value class Person(private val fullName: String) {
+	init {
+		require(fullName.isNotEmpty()) {
+			"Full name shouldn't be empty"
+		}
+	}
+
+	constructor(firstName: String, lastName: String) : this("$firstName $lastName") {
+		require(lastName.isNotBlank()) {
+			"Last name shouldn't be empty"
+		}
+	}
+
+	val length: Int
+		get() = fullName.length
+
+	fun greet() {
+		println("Hello, $fullName")
+	}
+}
+
+fun main() {
+	val name1 = Person("Thought", "Works")
+	val name2 = Person("Siddarth Iyer")
+	name1.greet() // the `greet()` function is called as a static method
+	println(name2.length) // property getter is called as a static method
+}
